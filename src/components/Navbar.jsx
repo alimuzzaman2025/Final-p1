722451037c05e6168fee5092205f86c9200ac9ca
A. K. M ALIMUZZAMAN <alimuzzaman2468@gmail.com>
2025-08-06 03:17:36 +0600

    demo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,66 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Container from './Container'
 import { FaBarsStaggered, FaUser } from 'react-icons/fa6'
 import { IoMdArrowDropdown } from 'react-icons/io'
 import { IoCartSharp, IoSearch } from 'react-icons/io5'
+import Crtboximage from "../assets/cart-box-image.png"
+import { RxCross2 } from 'react-icons/rx'
 
 const Navbar = () => {
+    let [category, setCategory] = useState(false)
+    let [accShow, setAccShow] = useState(false)
+    let [cartShow, setCartShow ] = useState(false)
+    let cartRef = useRef()
+    let cateRef = useRef();
+    let accRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener("click", (e) => {
+            if (cateRef.current.contains(e.target)) {
+                setCategory(!category)
+
+            } else {
+                setCategory(false)
+            }
+            if (accRef.current.contains(e.target)) {
+                setAccShow(!accShow)
+            } else {
+                setAccShow(false)
+
+            }
+            if (cartRef.current.contains(e.target)) {
+                setCartShow(!cartShow)
+            } else {
+                setCartShow(false)
+
+            }
+
+        })
+    }, [category, accShow,cartShow])
+
+
     return (
         <section className='bg-[#F5F5F3] py-3'>
             <Container>
                 <div className='flex items-center'>
-                    <div className='w-1/4'>
-                        <div className='flex items-center'>
+                    <div className='w-1/4 relative'>
+                        <div className='flex items-center' ref={cateRef}>
                             <FaBarsStaggered />
                             <h2 className='pl-2'>Shop by Category</h2>
                         </div>
+                        {category &&
+
+                            <div className='bg-[#262626] py-3 absolute top-[50px] left-0 w-full  group-hover:opacity-100 z-[2]'>
+                                <ul>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Accesories</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Furniture</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Electronics</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Clothes</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Bags</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3 hover:pl-6 duration-300 ease-in-out hover:text-[#fff] py-2'><a href="">Home appliances</a></li>
+                                </ul>
+                            </div>
+                        }
                     </div>
                     <div className='w-2/4'>
                         <div className='relative'>
@@ -23,14 +70,57 @@ const Navbar = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='w-1/4'>
-                        <div className='flex justify-end'>
-                            <div className='flex'>
+                    <div className='w-1/4 relative'>
+                        <div className='flex justify-end gap-5'>
+                            <div className='flex' ref={accRef}>
                                 <FaUser />
                                 <IoMdArrowDropdown />
                             </div>
-                            <IoCartSharp />
+                            <div className='' ref={cartRef}>
+                                <IoCartSharp />
+                            </div>
                         </div>
+                        {accShow &&
+                            <div className='w-[200px] absolute top-[48px] left-[100px] z-[2]'>
+                                <ul>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3  py-2 b hover:bg-[#fff] hover:text-[#000] bg-[#262626] text-center'><a href="">My Account</a></li>
+                                    <li className='text-[rgba(240,233,233,0.5)] text-[14px] font-dm font-normal pl-3  py-2 b hover:bg-[#fff] hover:text-[#000] bg-[#262626] text-center'><a href="">Log Out</a></li>
+
+                                </ul>
+                            </div>
+                        }
+                        {cartShow &&
+                        <div className='w-[270px] absolute top-[48px] left-[62px] py-2 z-[2]'>
+                            <div>
+                                <div className='flex gap-3 items-center pl-2 bg-[#F5F5F3] py-2'>
+                                    <div className='w-[80px] h-[80px]'>
+                                        <img src={Crtboximage} alt="" />
+                                    </div>
+                                    <div className='font-dm font-bold text-[14px] text-[#262626]'>
+                                        <p >Black Smart Watch</p>
+                                        <p>$44.00</p>
+                                    </div>
+                                    <div className='justify-end pl-2'>
+                                        <RxCross2 />
+                                    </div>
+                                </div>
+                                <div className=''>
+                                    <div className='flex items-center gap-1 py-[14px] pl-2'>
+                                        <p>Subtotal:</p>
+                                        <p className='font-dm font-bold text-[14px] text-[#262626]'> $44.00</p>
+                                    </div>
+                                    <div className='flex px-2 py-[20px] gap-5 justify-between'>
+                                        <div className=''>
+                                            <button className=''><a className="py-2 px-5 border-2 border-[#000] bg-[#000] text-[#fff] hover:bg-[#fff] hover:text-[#000]" href="">View Cart</a></button>
+                                        </div>
+                                        <div className=''>
+                                            <button className=''><a className="py-2 px-5 border-2 border-[#000] bg-[#000] text-[#fff] hover:bg-[#fff] hover:text-[#000]" href="">Checkout</a></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        }
                     </div>
                 </div>
             </Container>
@@ -39,4 +129,7 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Information from '../components/Information'
+import Ads from '../components/Ads'
 
 const Home = () => {
     return (
         <>
             <Header />
             <Navbar />
+            <Banner />
+            <Information />
+            <Ads />
         </>
     )
 }
